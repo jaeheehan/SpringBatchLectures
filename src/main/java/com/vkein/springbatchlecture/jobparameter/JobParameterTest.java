222dc804bb6ab49f7f65c9faabb04f4d43875408
jaehee <jaehee23@naver.com>
2022-05-28 20:24:41 +0900

    Job Parameter TEST

--- src/main/java/com/vkein/springbatchlecture/jobparameter/JobParameterTest.java
+++ src/main/java/com/vkein/springbatchlecture/jobparameter/JobParameterTest.java
@@ -1,7 +1,6 @@
-package com.vkein.springbatchlecture;
+package com.vkein.springbatchlecture.jobparameter;
 
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
@@ -10,8 +9,10 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
 @Component
-public class JobRunner implements ApplicationRunner {
+public class JobParameterTest implements ApplicationRunner {
 
     @Autowired
     private JobLauncher jobLauncher;
@@ -19,14 +20,17 @@ public class JobRunner implements ApplicationRunner {
     @Autowired
     private Job job;
 
-
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
         JobParameters jobParameters = new JobParametersBuilder()
-                .addString("name", "user2")
+                .addString("name", "user1")
+                .addLong("seq", 2L)
+                .addDate("date", new Date())
+                .addDouble("age", 16.5)
                 .toJobParameters();
 
         jobLauncher.run(job, jobParameters);
+
     }
 }
