23fd7077f40424ad31b6323e6c5b517c33582cb8
jaehee <jaehee23@naver.com>
2022-05-28 16:17:03 +0900

    Initial commit

---
+++ src/test/java/com/vkein/springbatchlecture/SpringBatchLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vkein.springbatchlecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchLectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
